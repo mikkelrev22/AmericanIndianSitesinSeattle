91754b75d82ccc7e4d14b8f02f791032a2b81473
Michael Thompson <michael.220488@gmail.com>
2019-12-10 16:00:15 -0800

    thinking about components

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,7 +1,25 @@
 import {Component} from 'react'
+import {Map, GoogleApiWrapper} from 'google-maps-react'
 
 class App extends Component {
   constructor () {
-    
+    super ()
+    this.state = {
+
+    }
+  }
+  render () {
+    return (
+      <div>
+        <Map
+        google={this.props.google}
+        zoom={8}
+        style={mapStyles}
+        initialCenter={{lat: 47.6062, lng: -122.3321}}
+        />
+      </div>
+    )
   }
 }
+
+export default App
